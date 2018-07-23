aaf57a0d1592e6dc991d264b9bba5315b705c4e7
sonyapieklik <piesonya@gmail.com>
2018-07-23 18:13:21 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,14 +1,14 @@
 # Code your prompts here!
 puts "guest name"
-guest_name = gets.chomp.captilize!
+guest_name = gets.chomp.capitalize
 puts "party name"
-party_name = gets.chomp.captilize
+party_name = gets.chomp.capitalize
 puts "date"
-date = gets.chomp.captilize
+date = gets.chomp.capitalize
 puts "time"
-time = gets.chomp.captilize
+time = gets.chomp.capitalize
 puts "host name"
-host_name = gets.chomp.captilize
+host_name = gets.chomp.capitalize
 
 # Try starting out with puts'ing a string.
 "Dear #{guest_name},
