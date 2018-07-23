4f822c1042a2531ed39ca1fd83378d2d61224da4
sonyapieklik <piesonya@gmail.com>
2018-07-23 18:37:46 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,6 @@
 # Code your prompts here!
 puts "Hi, you've been invited to a party! What is your name?"
-guest_name = gets.chomp.capitalize
+guest_name = gets.split(' ').map {|w| w.capitalize }.join(' ')
 puts "party name"
 party_name = gets.chomp.capitalize
 puts "date"
