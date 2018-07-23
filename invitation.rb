0568d9ceeb300b72a7b15db1fd56ef269ef2eede
sonyapieklik <piesonya@gmail.com>
2018-07-23 18:14:46 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -11,7 +11,7 @@ puts "host name"
 host_name = gets.chomp.capitalize
 
 # Try starting out with puts'ing a string.
-"Dear #{guest_name},
+puts "Dear #{guest_name},
  
 You are cordially invited to the #{party_name} on #{date} at #{name}. Please RSVP no later than #{date}.
  
